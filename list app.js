6bd6dcaf4a6fbafaa5c3d562fdfe54601456514d
Saran2007-prog <stalinsaran2020@gmail.com>
2025-09-18 10:21:10 +0530

    Add files via upload

---
+++ list app.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.getElementById("listitem").style.width = "250px";
+    document.getElementById("main").style.marginleft="250px";
+}
+function ClossNav() {
+    document.getElementById("listitem").style.width = "0";
+    document.getElementById("main").style.marginleft = "0";  
+}
